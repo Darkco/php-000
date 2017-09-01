83727e3623cfef6fb620f06c3f5da1b33dfac595
Darkco <darkcobeans@yahoo.com>
2017-09-01 00:34:59 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script>
+            $(document).ready(function(){                        
+            var $input = $("#inputTexter");                               
+            $input.on('input', function() {
+                //Selected value
+                var inputValue = $(this).val();
+                //Ajax for calling php function
+                $.post('contentimg.php', { inputValueV: inputValue }, function(data){
+                    $("#contentImg").html(data);
+                    //do after submission operation in DOM
+                });
+            });
+        });
+</script>
+<title>Campaign Setup</title>
+ <style>
+ div,input {margin:0; padding:0;}
+ </style>
+</head>
+<body>
+<?php
+echo '<form method="post" action="" >';
+echo '<input id="inputTexter" name="inputTexter" type="text" name="inputValueV"  /><br />';
+echo '</form>';
+echo '<div id="contentImg"> </div>';
+?>
+</body>
+</html>
